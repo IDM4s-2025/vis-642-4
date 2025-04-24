e4c92b0bbd5879273722b31eafb186bb5728e75c
Dilosch03 <diegolosch@gmail.com>
2025-04-23 18:08:12 -0600

    se modifico los parametros para el scatter

--- diego_func.py
+++ diego_func.py
@@ -2,5 +2,5 @@ from plotly import express as px
 import pandas as pd
 
 def diego_func(df:pd.DataFrame):
-    fig = px.scatter(df, x="SepalWidthCm", y="SepalLengthCm", color="Species")
+    fig = px.scatter(df, x="SepalLengthCm", y="PetalLengthCm", color="Species")
     fig.show()
